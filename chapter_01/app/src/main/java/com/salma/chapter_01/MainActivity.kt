4f05888b700f4387e74510f021e70e75f91eeca0
= <=>
2022-06-25 08:03:19 +0200

    Implemented Chapter 1, Which Covers - Composable functions - Preview Feature

--- chapter_01/app/src/main/java/com/salma/chapter_01/MainActivity.kt
+++ chapter_01/app/src/main/java/com/salma/chapter_01/MainActivity.kt
@@ -3,41 +3,122 @@ package com.salma.chapter_01
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import com.salma.chapter_01.ui.theme.Chapter_01Theme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Chapter_01Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Hello()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Hello() {
+    val name = remember {
+        mutableStateOf("")
+    }
+    val nameEntered = remember {
+        mutableStateOf(false)
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp), contentAlignment = Alignment.Center
+
+    ) {
+        if (nameEntered.value) {
+            Greeting(name = name.value)
+        } else {
+            Column(
+                modifier = Modifier.padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Welcome()
+                TextAndButton(name = name, nameEntered = nameEntered)
+            }
+        }
+    }
+
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    Chapter_01Theme {
-        Greeting("Android")
+@Preview
+fun Welcome() {
+    Text(
+        text = stringResource(id = R.string.welcome),
+        style = MaterialTheme.typography.subtitle1,
+    )
+}
+
+
+@Composable
+fun TextAndButton(name: MutableState<String>, nameEntered: MutableState<Boolean>) {
+
+    Row(modifier = Modifier.padding(8.dp)) {
+        TextField(
+            value = name.value,
+            onValueChange = {
+                name.value = it
+            },
+            placeholder = {
+                Text(text = stringResource(id = R.string.hint))
+            },
+            singleLine = true,
+            modifier = Modifier
+                .alignByBaseline()
+                .weight(1.0F),
+            keyboardOptions = KeyboardOptions(
+                autoCorrect = false,
+                capitalization = KeyboardCapitalization.Words
+            ),
+            keyboardActions = KeyboardActions(
+                onAny = {
+                    nameEntered.value = true
+                }
+            )
+        )
+
+        Button(
+            onClick = { nameEntered.value = true },
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Text(text = stringResource(id = R.string.done))
+        }
     }
+}
+
+//@Composable
+//@Preview
+//fun GreetingWrapper(){
+//    Greeting(name = "static name")
+//}
+
+
+@Composable
+@Preview
+fun Greeting(name: String= "no one") {
+    Text(
+        text = stringResource(id = R.string.greeting, name),
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.subtitle1
+    )
 }
